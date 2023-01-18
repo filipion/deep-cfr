8cda634f7f7bd6fc7633f953aeb975e02c6bd90b
filipion <filip.alexandru.ion@gmail.com>
2023-01-18 10:48:27 +0200

    Epsilon constant

--- kuhn_poker_test.py
+++ kuhn_poker_test.py
@@ -1,31 +1,32 @@
 from cfr_kuhn import *
 
+epsilon = 1e-2
+
 def testKuhnPoker():
-    epsilon = 1e-2
     iterations = 400000
     train(iterations)
     strategy = {key : val.getAverageStrategy() for key, val in nodeMap.items()}
-    testKuhnStrategy(strategy, epsilon)
+    testKuhnStrategy(strategy)
 
-def testVectorEquality(v, w, epsilon):
+def testVectorEquality(v, w):
     for x, y in zip(v, w):
         assert x < y + epsilon and x > y - epsilon
 
-def testKuhnStrategy(strategy, epsilon):
-    testVectorEquality(strategy['3p'], [0, 1], epsilon)
-    testVectorEquality(strategy['3b'], [0, 1], epsilon)
-    testVectorEquality(strategy['2p'], [1, 0], epsilon)
-    testVectorEquality(strategy['2b'], [2/3, 1/3], epsilon)
-    testVectorEquality(strategy['1p'], [2/3, 1/3], epsilon)
-    testVectorEquality(strategy['1b'], [1, 0], epsilon)
+def testKuhnStrategy(strategy):
+    testVectorEquality(strategy['3p'], [0, 1])
+    testVectorEquality(strategy['3b'], [0, 1])
+    testVectorEquality(strategy['2p'], [1, 0])
+    testVectorEquality(strategy['2b'], [2/3, 1/3])
+    testVectorEquality(strategy['1p'], [2/3, 1/3])
+    testVectorEquality(strategy['1b'], [1, 0])
 
     alpha = strategy['1'][1]
-    testVectorEquality(strategy['1'], [1 - alpha, alpha], epsilon)
-    testVectorEquality(strategy['2'], [1, 0], epsilon)
-    testVectorEquality(strategy['3'], [1 - 3 * alpha, 3 * alpha], epsilon)
-    testVectorEquality(strategy['1pb'], [1, 0], epsilon)
-    testVectorEquality(strategy['2pb'], [2/3 - alpha, 1/3 + alpha], epsilon)
-    testVectorEquality(strategy['3pb'], [0, 1], epsilon)
+    testVectorEquality(strategy['1'], [1 - alpha, alpha])
+    testVectorEquality(strategy['2'], [1, 0])
+    testVectorEquality(strategy['3'], [1 - 3 * alpha, 3 * alpha])
+    testVectorEquality(strategy['1pb'], [1, 0])
+    testVectorEquality(strategy['2pb'], [2/3 - alpha, 1/3 + alpha])
+    testVectorEquality(strategy['3pb'], [0, 1])
 
 
 testKuhnPoker()
